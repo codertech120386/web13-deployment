c5012b9ce54b485c890d18c2bd77c8939ad0bb58
Dhaval Chheda <dhavalchheda@Dhavals-MacBook-Pro.local>
2022-01-12 15:30:30 +0530

    changed db from localhost to atlas

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://jaskaran:jaskaran98033@cluster0.kzv4y.mongodb.net/web13-ejs"
+    "mongodb+srv://jaskaran:jaskaran@cluster0.kzv4y.mongodb.net/web13-ejs"
   );
 };
